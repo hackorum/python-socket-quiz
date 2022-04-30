1ef41aff7af77068a3d8fa3ca7c0a638ea5dccc0
Neil Sabde <neilsabde@Neils-MacBook-Pro.local>
2022-04-30 12:45:52 +0530

    Third Commit

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import socket
 from threading import Thread
+from tkinter import *
 
-nickname = input("Enter your nickname: ")
+# nickname = input("Enter your nickname: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -10,29 +11,57 @@ port = 8080
 
 client.connect((ip_address, port))
 
+class GUI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.withdraw()
 
-def receive():
-    while True:
-        try:
-            message = client.recv(2048).decode("utf-8")
-            if message == "NICKNAME":
-                client.send(nickname.encode("utf-8"))
-            else:
-                print(message)
-        except:
-            print("An error occured")
-            client.close()
-            break
+        self.login = Toplevel()
+        self.login.title("Login")
+        self.login.resizable(width=False, height=False)
+        self.login.configure(width=400, height=300)
+        
+        self.pls = Label(self.login, text="Please login to continue", justify=CENTER, font="Helvetica 14 bold")
+        self.pls.place(relheight=0.15, relx=0.2, rely=0.07)
+
+        self.entryName = Entry(self.login, font="Helvetica 14")
+        self.entryName.place(relwidth=0.4, relheight=0.12, relx=0.35, rely=0.2)
+        self.entryName.focus()
+
+        self.go = Button(self.login, text="CONTINUE", font="Helvetica 14 bold", command=lambda: self.goAhead(self.entryName.get()))
+        self.go.place(relx=0.4, rely=0.55)
+
+        self.window.mainloop()
+    def receive(self):
+        while True:
+            try:
+                message = client.recv(2048).decode("utf-8")
+                if message == "NICKNAME":
+                    client.send(nickname.encode("utf-8"))
+                else:
+                    pass
+            except:
+                print("An error occured")
+                client.close()
+                break
+    def goAhead(self, name):
+        self.login.destroy()
+        self.name = name
+
+        receive_thread = Thread(target=self.receive)
+        receive_thread.start()
+        
+
+g = GUI()
 
 
-def write():
-    while True:
-        message = "{}:{}".format(nickname, input(""))
-        client.send(message.encode("utf-8"))
+
+# def write():
+#     while True:
+#         message = "{}:{}".format(nickname, input(""))
+#         client.send(message.encode("utf-8"))
 
 
-receive_thread = Thread(target=receive)
-receive_thread.start()
 
-write_thread = Thread(target=write)
-write_thread.start()
+# write_thread = Thread(target=write)
+# write_thread.start()
